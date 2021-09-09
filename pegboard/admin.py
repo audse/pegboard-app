e85464a958b99351f459e1979d2b4a1fde7e2907
Audrey Serene <audrey.serene@gmail.com>
2021-09-09 10:59:20 -0500

    <setup> Iinitialize git

---
+++ pegboard/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from .models import Card
+
+# Register your models here.
+
+admin.site.register(Card)
